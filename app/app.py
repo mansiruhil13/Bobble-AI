8b6678d321a978d95e20485b8e7593e08a851b58
ashupal86 <mr.ashu1810@gmail.com>
2024-10-12 00:03:55 +0530

    fix: routes | feat: database models can be added as per need, admin control can be implmented, secured forms, simple project structure | bug: use jinja templating to all the other file and use bootstrap for css and js elements

--- app/app.py
+++ app/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
-import sqlite3
 from flask_sqlalchemy import SQLAlchemy
+from routes import register_routes
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 db = SQLAlchemy(app)
+
+register_routes(app)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
