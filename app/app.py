ef7f83877d39f460ffcf19d2cd4f591e7a37666d
ashupal86 <mr.ashu1810@gmail.com>
2024-10-12 15:48:59 +0530

    Document the project for other devs

--- app/app.py
+++ app/app.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes import register_routes
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secretkey'
 
 
 db = SQLAlchemy(app)
 
 register_routes(app)
+
+migrate = Migrate(app, db)  
+
 
 
 
